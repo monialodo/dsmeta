4095509d41fb116f5a67b6b87ce096052f8c8be4
monia.lodo <NW65UQ3KQU5FkjCGuupA>
2022-11-15 16:45:27 -0300

    First Component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,18 @@
+import icon from '../../assets/img/avatar-icon.svg';
+import './styles.css';
+
+function NotificationButton() {
+
+  return (
+
+    <div className="dsmeta-red-btn">
+      <img src= {icon} alt="Notificar" />
+    </div>
+
+
+  )
+
+
+}
+
+export default NotificationButton
