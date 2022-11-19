d81355e1b5e873e21c7216143fcecf8c2376e17e
monia.lodo <NW65UQ3KQU5FkjCGuupA>
2022-11-19 20:30:58 -0300

    Date Select

--- backend/src/main/java/com/devsuperior/dsmeta/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/service/SaleService.java
@@ -3,9 +3,13 @@ package com.devsuperior.dsmeta.service;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleService {
@@ -13,9 +17,13 @@ public class SaleService {
     @Autowired
     private SaleRepository repository;
 
-    public List<Sale> findSales() {
+    public Page<Sale> findSales(Pageable pageable, String minDate, String maxDate) {
 
-        return repository.findAll();
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+        LocalDate min =  minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today: LocalDate.parse(maxDate);
+
+        return repository.findSales(min, max, pageable);
 
     }
 }
